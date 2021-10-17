dbb276803afce4835ca936cf4521026efd905124
KaranJain007 <Karan@cyntexa.com>
2021-10-17 18:50:41 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -67,8 +67,8 @@ exports.edit = function (req, res) {
     
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
-    logData(req);
     console.log("coming in exports of edit");
+    logData(req);
     res.send(200, 'Edit');
 };
 
@@ -115,16 +115,98 @@ exports.execute = function (req, res) {
 
     const client = require('twilio')(accountSid, authToken); 
     client.messages 
-        
-          .create({ 
+        .create({ 
              body: body,
             statusCallback: 'http://postb.in/1634376692313-1793774713296',
-             messagingService: messagingService,
              from :'+19156420620' ,
              to: '+91' + to
              
            }) 
-           .then(message => console.log(message.sid))
+           .then(message => { 
+            console.log(message);
+
+
+            //package ka authendpoint
+            var authEndpoint = "mc6vgk-sxj9p08pqwxqz9hw9-4my.auth.marketingcloudapis.com" 
+
+
+            const data = JSON.stringify({
+                client_id: "a99und2ssv38obcjqoe8v1mp", //pass Client ID
+                client_secret: "mt7J90uglmROiZV62dGmZYl9", //pass Client Secret
+                grant_type: "client_credentials"
+            })
+
+            const options = {
+                hostname: authEndpoint,
+                path: '/v2/token',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                  //  'Content-Length': data.length
+                }
+            }
+            var accessToken = '';
+            var restURL = '';
+            const requestForToken = http.request(options, res => {
+                console.log(`statusCode: ${res.statusCode}`)
+                var jsonString = '';
+                res.on('data', d => {
+                    jsonString += d;
+                    process.stdout.write(d)
+                })
+                res.on('end', function() {
+                    var resData = JSON.parse(jsonString);
+                    accTok += resData.access_token
+                    restURL += resData.rest_instance_url
+                    console.log(`Access Token : ` + accessToken); 
+                    console.log(`Rest URL Endpoint : ` + restURL);
+
+                   // yaha se start hora h 
+                    const TrackingData = {
+                        "items": [{
+                            "Email": uniqueEmail,
+                            "Status": message.status,
+                            "AccountSID": message.accountSid,
+                            "apiVersion": message.apiVersion,
+                            "Body": message.body,
+                            "dateCreated": message.dateCreated,
+                            "dateUpdated": message.dateUpdated,
+                            "dateSent": message.dateSent,
+                            "direction": message.direction,
+                            "from": message.from,
+                            "messagingServiceSid": message.messagingServiceSid,
+                            "price": message.price,
+                            "priceUnit": message.priceUnit,
+                            "sid": message.sid,
+                            "uri": message.uri
+                        }]
+                    }
+                    console.log(TrackingData);
+                    console.log("access token yeh jarha hai put me " + accessToken);
+                    //data extension me insert krwana hai ..
+                    request.put({
+                        headers: { 'content-type': 'application/json', 'Authorization': 'Bearer ' + accessToken },
+                        url: restURL + '/data/v1/async/dataextensions/key:5D350EDF-D094-40F2-AE16-67FED6CA999D/rows',
+                        body: TrackingData,
+                        json: true
+                    }, function(error, response, body) {
+                        console.log(error);
+                        console.log("resultMessages" + body.resultMessages);
+                        console.log("resultMessages" + response.requestId);
+                    });
+                    
+                })
+            })
+            requestForToken.on('error', error => {
+                console.error(error);
+            })
+            requestForToken.write(data);
+            requestForToken.end();
+
+            
+
+            console.log(message)
+        })
           .done();
 
 
@@ -170,7 +252,7 @@ exports.publish = function (req, res) {
     //console.log("Published: "+req.body.inArguments[0]);        
     
     // Data from the req and put it in an array accessible to the main app.
-    console.log( req.body );
+    //console.log( req.body );
      logData(req);
      res.send(200, 'Publish');
      console.log("coming to publish");
