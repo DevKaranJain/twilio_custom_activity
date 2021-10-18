6899de3da8d9c0736eea36ce6b4e05567e27dabc
KaranJain007 <Karan@cyntexa.com>
2021-10-18 11:24:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ app.post('/journeybuilder/save/', activity.save );
 app.post('/journeybuilder/validate/', activity.validate );
 app.post('/journeybuilder/publish/', activity.publish );
 app.post('/journeybuilder/execute/', activity.execute );
-
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
