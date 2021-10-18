40bebb3f4f206bbafe2f95a39d9a5dd82aba8cbb
KaranJain007 <Karan@cyntexa.com>
2021-10-18 15:01:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.post('/login', routes.login );
 app.post('/logout', routes.logout );
 
 // Custom Hello World Activity Routes
-app.post('/journeybuilder/save', activity.save);
+app.post('/journeybuilder/save/', activity.save);
 app.post('/journeybuilder/validate/', activity.validate );
 app.post('/journeybuilder/publish/', activity.publish );
 app.post('/journeybuilder/execute/', activity.execute );
