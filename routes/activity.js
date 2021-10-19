2eb43616663e6291d826995f4aa4aa2657761c1a
KaranJain007 <Karan@cyntexa.com>
2021-10-19 16:03:52 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -119,7 +119,7 @@ exports.execute = function (req, res) {
              body: body,
              statusCallback: 'https://postb.in/1634632766316-9085072900634',
              from :'+19156420620' ,
-             to:('+91'+to)
+             To:('+91'+to.toString())
              }) 
            .then(message => console.log(message.sid)) 
             .done();
