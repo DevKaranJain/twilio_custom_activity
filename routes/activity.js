0879124a247ba29a16f9f37153b68c4042282396
KaranJain007 <Karan@cyntexa.com>
2021-10-19 11:49:27 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -117,7 +117,7 @@ exports.execute = function (req, res) {
     client.messages.create({ 
              body: body,
              from :'+19156420620' ,
-             to:'+918619396499'
+             to:'+91'+to
              }) 
            .then(message => console.log(message.sid)) 
             .done();
